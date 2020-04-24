debc37eb474d191b963bbcfd23c9918ea2651e2a
Mustafa mahmoud <masterseen45@gmail.com>
2020-04-24 13:57:27 +0300

    Adding xrays permission to lambda

--- c4-final-project-starter-code/backend/src/datalayer/todoAccess.ts
+++ c4-final-project-starter-code/backend/src/datalayer/todoAccess.ts
@@ -1,11 +1,15 @@
 import * as AWS  from 'aws-sdk'
 import * as AWSXRay from 'aws-xray-sdk'
 import { DocumentClient } from 'aws-sdk/clients/dynamodb'
+import { createLogger } from '../utils/logger';
 
 const XAWS = AWSXRay.captureAWS(AWS)
 const s3 = new AWS.S3({
   signatureVersion: 'v4'
 })
+
+const logger = createLogger('todo')
+
 
 import { TodoItem } from '../models/TodoItem'
 
@@ -118,12 +122,13 @@ export class TodoAccess {
 
 function createDynamoDBClient() {
   if (process.env.IS_OFFLINE) {
-    console.log('Creating a local DynamoDB instance')
+    logger.info("Creating Todos DynamoDB Client...");
     return new XAWS.DynamoDB.DocumentClient({
       region: 'localhost',
       endpoint: 'http://localhost:8000'
     })
   }
+  logger.info("Creating Todos DynamoDB Client...");
 
   return new XAWS.DynamoDB.DocumentClient()
 }
